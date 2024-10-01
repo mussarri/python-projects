2a4565838647cb4e67718e55e7f84b830addf808
musarri <msaricicek_99@hotmail.com>
2024-10-02 01:19:23 +0300

    word replacement

---
+++ word_replacement/app.py
@@ -0,0 +1,9 @@
+def replace_word():
+    str = "Hello my name is Mustafa, hi"
+    print(str)
+    word_to_replace = input("Enter word to replace: ")
+    word_replacement = input("Enter word replacement: ")
+    print(str.replace(word_to_replace,word_replacement))
+
+replace_word()
+    
